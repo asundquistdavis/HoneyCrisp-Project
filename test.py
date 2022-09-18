625a04c50058703226b291f5b6f0ba112438bbe8
Andrew <asundquistdavis@gmail.com>
2022-09-18 10:43:17 -0500

    reqs

--- test.py
+++ test.py
@@ -1,7 +1,16 @@
-from apples_db import add_to_db, query_db
+# from apples_db import add_to_db, query_db
 
-add_to_db('phrase', phrase='Hello this is a test as well', playername='Carissa')
-for p in query_db('phrase', playername='Carissa'):
-    print(f'Phrase: {p.phrase}\nUser: {p.playername}')
+# add_to_db('phrase', phrase='Hello this is a test as well', playername='Carissa')
+# for p in query_db('phrase', playername='Carissa'):
+#     print(f'Phrase: {p.phrase}\nUser: {p.playername}')
+
+with open('./requirements.txt', 'r') as r:
+    with open('./requirements_2.txt', 'w') as w:
+        s = r.readline().split()
+        while len(s) > 0:
+            l = s[0]+'=='+s[1]+'\n'
+            w.write(l)
+            s = r.readline().split()
 
 
+        
